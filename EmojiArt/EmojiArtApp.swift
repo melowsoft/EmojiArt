e42e99ec617f8196fee7dddefdb501208c353b7b
Anthony Okpako <melowsworld@yahoo.co.uk>
2022-02-01 05:43:54 +0100

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Tony on 01/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
